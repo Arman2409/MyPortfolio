2d634085376d3d552327f851a79d5c8c44c1811f
Arman <armanghazaryan2409@gmail.com>
2022-11-17 12:32:06 +0400

    build added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import fs from "fs";
 
 import sendMessage from "./controllers/sendMessage.js";
 import getData from "./controllers/getData.js";
@@ -20,6 +21,10 @@ app.use((req, res, next) => {
 });
 app.use(express.static(path.join(path.resolve() + "../frontend/build/")))
 
+fs.readFile(path.join(path.resolve() + "../frontend/src/App.js"), (err,data) => {
+    if (err) console.error(err);
+    else if (data) console.log(data);
+})
 // controllers 
 
 app.get('/', (req, res) => res.send('Server running!'));
