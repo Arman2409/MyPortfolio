ef6ce43baae9d644f27ec5beda2298683dda9cca
Arman2409 <arman.ghazaryan@hylink.am>
2023-12-25 18:41:10 +0400

    About component added

---
+++ src/components/About/About.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles/About.module.scss";
+
+const About = () => {
+    return (
+       <div className={styles.about_cont}>
+           <div className={styles.about_content}>
+             
+           </div>
+       </div>
+    )
+}
+
+export default About;
