f169b673764b413a09147e81e0c081054f4387d7
Arman2409 <arman.ghazaryan@hylink.am>
2024-01-01 23:47:24 +0400

    Logo click added

--- src/app/fixed/Header/Header.tsx
+++ src/app/fixed/Header/Header.tsx
@@ -1,11 +1,22 @@
+"use client"
+import { useCallback } from "react";
+
 import styles from "./styles/Header.module.scss";
 import Menu from "./components/Menu/Menu";
 
 const Header = () => {
+    const clickLogo = useCallback(() => window.scrollTo({
+        top: 0
+    }), [])
+
     return (
-        <div className={styles.header}>
-            <div className={styles.logo_cont}>
-               <img src="/logo.png" />
+        <div
+            className={styles.header}>
+            <div
+                className={styles.logo_cont}  >
+                <img
+                    src="/logo.png"
+                    onClick={clickLogo} />
             </div>
             <Menu />
         </div>
